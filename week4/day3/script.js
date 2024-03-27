42c18a3fbdb45ba74709e4d4ce4f18c36169fc02
alpayabdullayev <alpayabdullayev0@gmail.com>
2024-03-27 13:35:22 +0400

    js

---
+++ week4/day3/script.js
@@ -0,0 +1,49 @@
+// let userinsaniyesi=Number(prompt("Saniye daxil edin"))
+// let qaliq = userinsaniyesi%60
+// let deqiqe=parseInt(userinsaniyesi/60)
+// console.log(`${deqiqe} deqiqe ${qaliq} saniye`)
+
+
+// let usertemperatur=Number(prompt("temperatur daxil edin"))
+// let derece = 5/9 * (usertemperatur-32)
+// console.log(`${derece} derece`)
+
+
+
+
+// let fenn1=Number(prompt("num1"))
+// let fenn2=Number(prompt("num2"))
+// let fenn3=Number(prompt("num3"))
+// let fenn4=Number(prompt("num4"))
+// let fenn5=Number(prompt("num5"))
+// let orta=(fenn1+fenn2+fenn3+fenn4+fenn5)/5
+// console.log('${balortalamasi}:' +orta);
+
+
+// let sonil=Number(prompt("son ili daxil edin"))
+// let dogumili=Number(prompt("dogum ili daxil edin"))
+// let yas=sonil-dogumili
+// console.log(`${yas}`)
+
+
+// let ededinfaizi=Number(prompt("eded daxil edin"))
+// let faiz=Number(prompt("faiz eded daxil edin"))
+// let faizz=ededinfaizi*faiz/100
+// console.log(`${faizz}`)
+
+
+// let teref1=Number(prompt("katet1"))
+// let teref2=Number(prompt("katet2"))
+// let sahe=teref1*teref2/2
+// console.log(`${sahe}`)
+
+
+
+// let kreditmiq=Number(prompt("miqdar"))
+// let kreditf=Number(prompt("faiz"))
+// let kreditmud=Number(prompt("muddet"))
+// let netice=(kreditmiq+kreditmiq/kreditf/100)/kreditmud
+// console.log(`${netice}`)
+
+
+
