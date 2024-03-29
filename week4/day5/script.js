8c4418e2d74141750fb37036ea9e24c73003f67e
alpayabdullayev <alpayabdullayev0@gmail.com>
2024-03-29 13:27:08 +0400

    js

---
+++ week4/day5/script.js
@@ -0,0 +1,72 @@
+// 16
+// let arr =[2,5,7,8,9,11]
+// count =0
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i]%2==0){
+//         count++
+//     }
+    
+// }
+// if(count=arr.length)
+
+//  {
+//     console.log(true)
+
+//  }
+//  else{
+//     console.log(false)
+//  }
+
+
+// 18
+
+// let deyisen1=(prompt("soz elave edin1:"))
+// let deyisen2=(prompt("herf elave edin2:"))
+// let herf
+//  for( let i=0;i<deyisen1.length;i++){
+//     if(deyisen1[i]==deyisen2){
+//          herf=true
+//     }
+//     else { herf = false}
+// }
+//  console.log(herf)
+
+
+// 19
+// let deyisen1=(prompt("soz elave edin1:"))
+// let deyisen2=(prompt("herf elave edin2:"))
+// count=0
+// for( let i=0;i<deyisen1.length;i++){
+//     if(deyisen1[i]==deyisen2){
+//         count++
+//     }
+// }
+// console.log(count)
+
+
+// 20 tam deyil
+// let deyisen1=(prompt("soz elave edin1:"))
+// let sait =(prompt("sait elave et"))
+// count=0
+// for( let i=0;i<deyisen1.length;i++){
+//     if(deyisen1[i]==sait){
+//   count++
+//     }
+// }
+// console.log(count)
+
+
+// 15
+// let arr =[2,5,7,8,9,11]
+// let multiple=1
+// let sum=0
+// for ( let i=0;i<arr.length;i++){
+//    multiple*=arr[i]
+
+//    if(multiple%2==0){
+//       sum+=arr[i]
+//    }else{
+//     sum=0
+//    }
+// }
+// console.log(sum)
