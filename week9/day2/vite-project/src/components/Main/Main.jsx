dead8a240624c895f2babeda3f7f90ff1e67e592
mesteliyeva493 <tu7s1elba@code.edu.az>
2024-04-30 13:39:08 +0400

    stylereact

---
+++ week9/day2/vite-project/src/components/Main/Main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Main.scss";
+function Main() {
+  return (
+    <>
+      <section className="sone">
+        <div className="wrapper">
+          <div className="cardo">
+            <div className="cardh">
+              <h1>Find yourself <br /> outside at a cozy Camp this month.</h1>
+            </div>
+            <div className="p1">
+            <p>  Habitant morbi tristique senectus et. Nec dui nunc mattis enim ut
+              tellus. Semper auctor neque vitae tempus quam pellentesque nec nam
+              aliquam.</p>
+            </div>
+            <div className="cardp">
+              <p>
+                Image from <a href="#">Freepik</a>
+              </p>
+            </div>
+            <div className="btn1">
+              <button> Learn More</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="">
+  
+      </section>
+    </>
+  );
+}
+
+export default Main;
