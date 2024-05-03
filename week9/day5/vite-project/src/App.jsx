65cffdc5c0961701f64510cf598846b808ad1b8a
mesteliyeva493 <tu7s1elba@code.edu.az>
2024-05-03 13:00:08 +0400

    class

---
+++ week9/day5/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { useState } from 'react'
+
+import './App.css'
+import Sidebar from './components/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Sidebar/>
+    </>
+  )
+}
+
+export default App
